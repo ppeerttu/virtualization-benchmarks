ccbda97217bd446a6497054dd7e7bfd7e42729b0
ppeerttu <perttu.karna@gmail.com>
2020-11-27 20:09:01 +0200

    Disable autologin

--- platforms/firecracker/setup_os.sh
+++ platforms/firecracker/setup_os.sh
@@ -18,27 +18,25 @@ apt-get install -y \
     vim \
     xinit xorg \
     alsa-utils \
-    sudo \
     netplan.io \
     --no-install-recommends
 
 useradd -m -p $(openssl passwd -1 $rootfs_password) $rootfs_username
 usermod -aG sudo $rootfs_username
-# passwd -d root
 
 echo "$hostname" > /etc/hostname
 echo "$hosts" > /etc/hosts
 
 echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
 
-# cp /usr/share/zoneinfo/$rootfs_tz /etc/localtime
-
-mkdir /etc/systemd/system/serial-getty@ttyS0.service.d/
-cat <<EOF > /etc/systemd/system/serial-getty@ttyS0.service.d/autologin.conf
-[Service]
-ExecStart=
-ExecStart=-/sbin/agetty --autologin root -o '-p -- \\u' --keep-baud 115200,38400,9600 %I $TERM
-EOF
+# Allow root user to autologin without password
+# passwd -d root
+# mkdir /etc/systemd/system/serial-getty@ttyS0.service.d/
+# cat <<EOF > /etc/systemd/system/serial-getty@ttyS0.service.d/autologin.conf
+# [Service]
+# ExecStart=
+# ExecStart=-/sbin/agetty --autologin root -o '-p -- \\u' --keep-baud 115200,38400,9600 %I $TERM
+# EOF
 
 cat <<EOF > /etc/netplan/99_config.yaml
 network:
