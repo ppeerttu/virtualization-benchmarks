ade36cb04d7eb75466b6ec8d9cd906255c482cb6
ppeerttu <perttu.karna@gmail.com>
2020-11-29 16:48:56 +0200

    Use root user

--- platforms/firecracker/setup_os.sh
+++ platforms/firecracker/setup_os.sh
@@ -5,7 +5,6 @@ set -ex
 apt-get update
 apt-get install -y \
     language-pack-en-base \
-    sudo \
     ssh \
     net-tools \
     ethtool \
@@ -20,10 +19,11 @@ apt-get install -y \
     alsa-utils \
     iperf3 \
     sysbench \
+    netplan.io \
     --no-install-recommends
 
-useradd -m -p $(openssl passwd -1 $rootfs_password) $rootfs_username
-usermod -aG sudo $rootfs_username
+# useradd -m -p $(openssl passwd -1 $rootfs_password) $rootfs_username
+# usermod -aG sudo $rootfs_username
 
 echo "$hostname" > /etc/hostname
 echo "$hosts" > /etc/hosts
@@ -31,21 +31,21 @@ echo "$hosts" > /etc/hosts
 echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
 
 # Allow root user to autologin without password
-# passwd -d root
-# mkdir /etc/systemd/system/serial-getty@ttyS0.service.d/
-# cat <<EOF > /etc/systemd/system/serial-getty@ttyS0.service.d/autologin.conf
-# [Service]
-# ExecStart=
-# ExecStart=-/sbin/agetty --autologin root -o '-p -- \\u' --keep-baud 115200,38400,9600 %I $TERM
-# EOF
+passwd -d root
+mkdir /etc/systemd/system/serial-getty@ttyS0.service.d/
+cat <<EOF > /etc/systemd/system/serial-getty@ttyS0.service.d/autologin.conf
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin root -o '-p -- \\u' --keep-baud 115200,38400,9600 %I $TERM
+EOF
 
-# cat <<EOF > /etc/netplan/99_config.yaml
-# network:
-#   version: 2
-#   renderer: networkd
-#   ethernets:
-#     eth0:
-#       dhcp4: true
-# EOF
+cat <<EOF > /etc/netplan/99_config.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      dhcp4: true
+EOF
 
-# netplan generate
+netplan generate
