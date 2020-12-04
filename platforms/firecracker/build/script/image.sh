3b059c079e195f758963c2677b5ec834401e7a92
ppeerttu <perttu.karna@gmail.com>
2020-12-04 15:40:52 +0200

    testing

--- platforms/firecracker/build/script/image.sh
+++ platforms/firecracker/build/script/image.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 set -ex
 
+mount_root=/rootfs
+
 rm -rf /output/*
 
 cp /root/linux-source-$KERNEL_SOURCE_VERSION/vmlinux /output/vmlinux
@@ -9,20 +11,17 @@ cp /root/linux-source-$KERNEL_SOURCE_VERSION/.config /output/config
 truncate -s 1G /output/image.ext4
 mkfs.ext4 /output/image.ext4
 
-mount /output/image.ext4 /rootfs
-debootstrap --include openssh-server,netplan.io,nano focal /rootfs http://archive.ubuntu.com/ubuntu/
-mkdir -p /rootfs/mnt/root
-mkdir -p /rootfs/mnt/script
-mount --bind /root /rootfs/mnt/root
-mount --bind /script /rootfs/mnt/script
+mkdir -p $mount_root
+mount /output/image.ext4 $mount_root
+debootstrap --include openssh-server,netplan.io,nano focal $mount_root http://archive.ubuntu.com/ubuntu/
 
-sleep 10
-chroot /rootfs /bin/bash /mnt/script/provision.sh
+mount --bind / $mount_root/mnt
 
-umount /rootfs/mnt/script
-# Somehow this does not work, so skip
-# umount /rootfs/mnt/root
-umount /rootfs
+pwd
+chroot $mount_root /bin/bash /mnt/script/provision.sh
+pwd
+umount $mount_root/mnt
+umount $mount_root
 
 cd /output
 tar czvf ubuntu-focal.tar.gz image.ext4 vmlinux config
