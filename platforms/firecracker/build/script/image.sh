e18e5292a4311e16a0d1140b15c499901439a09b
ppeerttu <perttu.karna@gmail.com>
2020-12-04 16:07:23 +0200

    fine tuning

--- platforms/firecracker/build/script/image.sh
+++ platforms/firecracker/build/script/image.sh
@@ -15,14 +15,19 @@ mkdir -p $mount_root
 mount /output/image.ext4 $mount_root
 debootstrap --include openssh-server,netplan.io,nano focal $mount_root http://archive.ubuntu.com/ubuntu/
 
-mount --bind / $mount_root/mnt
+mount --bind /root $mount_root/mnt/root
+mount --bind /script $mount_root/mnt/script
 
-pwd
 chroot $mount_root /bin/bash /mnt/script/provision.sh
-pwd
-umount $mount_root/mnt
+umount $mount_root/mnt/script
+umount $mount_root/mnt/root
 umount $mount_root
 
 cd /output
 tar czvf ubuntu-focal.tar.gz image.ext4 vmlinux config
 cd /
+
+I: Extracting zlib1g...
+W: Failure trying to run: chroot "/rootfs" mount -t proc proc /proc
+W: See /rootfs/debootstrap/debootstrap.log for details
+I: Installing core packages...
