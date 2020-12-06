d36dd9ab8ea739bcd82555f743f6f75d5c146835
ppeerttu <perttu.karna@gmail.com>
2020-12-06 11:42:54 +0200

    Resize rootfs

--- platforms/firecracker/config.sh
+++ platforms/firecracker/config.sh
@@ -6,3 +6,4 @@ firecracker_dir=~/.firecracker
 firecracker_version=$(basename $(curl -fsSLI -o /dev/null -w  %{url_effective} https://github.com/firecracker-microvm/firecracker/releases/latest))
 kernel_file=$firecracker_dir/ubuntu-vmlinux
 rootfs_file=$firecracker_dir/ubuntu-rootfs.ext4
+rootfs_final_size=5G
